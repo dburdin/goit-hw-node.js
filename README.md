161e1d83beaeee8ae8e2519e408309ddcdf68386
Dmytrii Bevziuk <113050865+dburdin@users.noreply.github.com>
2023-06-14 22:25:18 +0300

    adding try/catch for async functions + handling errors, adding gitignore file

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 hm-01 |
+
 1. List - https://monosnap.com/file/dGZnlOqrq0QtkUlPBu1d6ooBzC9F0P
 2. Get - https://monosnap.com/file/rT5YoewvYWB2G5qGBjOR3i1vwimZI1
 3. Add - https://monosnap.com/file/ah9qCas52O7qM81mgejBipAjarVAij
--- contacts.js
+++ contacts.js
@@ -6,35 +6,51 @@ const contactsPath = path.join("db", "contacts.json");
 
 // TODO: задокументувати кожну функцію
 async function listContacts() {
-  const contacts = JSON.parse(await readFile(contactsPath, "utf-8"));
-  return contacts;
+  try {
+    const contacts = JSON.parse(await readFile(contactsPath, "utf-8"));
+    return contacts;
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
 async function getContactById(contactId) {
   const id = String(contactId);
-  const contacts = await listContacts();
-  const contact = contacts.find((contact) => contact.id === id);
-  return contact || null;
+  try {
+    const contacts = await listContacts();
+    const contact = contacts.find((contact) => contact.id === id);
+    return contact || null;
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
 async function removeContact(contactId) {
-  const contacts = await listContacts();
-  const id = String(contactId);
-  const index = contacts.findIndex((contact) => contact.id === id);
-  if (index === -1) {
-    return null;
+  try {
+    const contacts = await listContacts();
+    const id = String(contactId);
+    const index = contacts.findIndex((contact) => contact.id === id);
+    if (index === -1) {
+      return null;
+    }
+    const result = contacts.splice(index, 1);
+    await writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return result;
+  } catch (error) {
+    console.log(error.message);
   }
-  const result = contacts.splice(index, 1);
-  await writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-  return result;
 }
 
 async function addContact(data) {
-  const contacts = await listContacts();
-  const newContact = { id: nanoid(), ...data };
+  try {
+    const contacts = await listContacts();
+    const newContact = { id: nanoid(), ...data };
 
-  contacts.push(newContact);
-  await writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-  return newContact || null;
+    contacts.push(newContact);
+    await writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return newContact || null;
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 module.exports = { listContacts, getContactById, addContact, removeContact };
